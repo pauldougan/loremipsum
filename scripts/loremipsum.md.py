a2266f3b7f0bfdd0b720026f9b45f98bd7a806a9
Paul Dougan <171561+dougapd@users.noreply.github.com>
2020-05-05 09:41:57 +0100

    adjust whitespace

--- scripts/loremipsum.md.py
+++ scripts/loremipsum.md.py
@@ -4,5 +4,5 @@ from faker import Faker
 f = Faker('la')
 
 for _ in range(3):
-    print(f"# {f.sentence().replace('.','')}\n")
+    print(f"# {f.sentence().replace(' .','')}\n")
     print(f"{f.paragraph(7)}\n")
